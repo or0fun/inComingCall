19ca84164f0653ca275ec8c183897e9022391274
fang <fangjun.nk@gmail.com>
2015-03-20 23:59:48 +0800

    init import

--- app/src/main/java/com/iflytek/util/Constant.java
+++ app/src/main/java/com/iflytek/util/Constant.java
@@ -6,11 +6,6 @@ package com.iflytek.util;
  */
 public class Constant {
 
-	/** 灵犀包名和启动activity */
-	public static final String PACKAGENAME_LINGXI = "com.iflytek.cmcc";
-	/** 灵犀启动activity */
-	public static final String STARTACTIVITY_LINGXI = "com.iflytek.viafly.Home";
-
 	// 振铃(RINGING),摘机(OFFHOOK)和空闲(IDLE) 
 	public static final String RINGING = "RINGING";
 	public static final String OFFHOOK = "OFFHOOK";
